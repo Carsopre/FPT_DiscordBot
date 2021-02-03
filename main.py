fcafefc472d2b27e5238e15542fade946048a5c1
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2021-02-03 09:59:32 +0100

    Modified method time_to_spam to have a max o messages per hour.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from keep_alive import keep_alive
+from typing import Optional, List
+from pydatinc import BaseClass as DataClass
 import discord
+import datetime
 import os
 
 # Gif libraries.
@@ -7,7 +10,29 @@ import random
 import json
 import requests
 
+
 client = discord.Client()
+discord_key = os.getenv("DISCORD_TOKEN")
+
+
+class SpamUser(DataClass):
+    discriminator: int
+    search_terms: List[str]
+    last_mssg: Optional[datetime.datetime]
+
+
+gabri_spam = SpamUser(
+    **{
+        "discriminator": os.getenv("gabri_discriminator"),
+        "search_terms": ["coffee", "spaghetti", "pasta", "pineapple pizza"],
+    }
+)
+tim_spam = SpamUser(
+    **{
+        "discriminator": os.getenv("tim_discriminator"),
+        "search_terms": ["dance", "fresh-prince", "he-man", "mary-poppins"],
+    }
+)
 
 
 def find_gif(search_term: str) -> str:
@@ -15,9 +40,7 @@ def find_gif(search_term: str) -> str:
     tenorgif_key = os.getenv("TENOR_TOKEN")
     lmt = 40
 
-    # our test search
-
-    # get the top 8 GIFs for the search term
+    # get the top "lmt" GIFs for the search term
     r = requests.get(
         f"https://api.tenor.com/v1/search?q={search_term}&key={tenorgif_key}&limit={lmt}"
     )
@@ -30,6 +53,33 @@ def find_gif(search_term: str) -> str:
     return ""
 
 
+async def time_to_spam(message):
+    spam_users: List[SpamUser] = [gabri_spam, tim_spam]
+    user_to_spam = next(
+        (
+            spam_user
+            for spam_user in spam_users
+            if spam_user.discriminator == message.author.discriminator
+        ),
+        None,
+    )
+    if not user_to_spam:
+        return
+
+    now_time: datetime.datetime = datetime.datetime.now()
+    if not user_to_spam.last_mssg or (
+        now_time - user_to_spam.last_mssg > datetime.timedelta(minutes=60)
+    ):
+        search_idx = random.randint(0, len(user_to_spam.search_items) - 1)
+        gif = find_gif(user_to_spam.search_items[search_idx])
+        default_mssg = f"Ey {message.author.mention} bring me coffee."
+        await message.channel.send(default_mssg)
+        if gif:
+            await message.channel.send(gif)
+        # Update timestamp.
+        user_to_spam.last_mssg = now_time
+
+
 @client.event
 async def on_ready():
     print("We have logged in as {0.user}".format(client))
@@ -40,22 +90,9 @@ async def on_message(message):
     if message.author == client.user:
         # Avoid infinite loop!
         return
-    user_discriminators = {
-        gabri_discriminator: ["coffee", "spaghetti", "pasta", "pineapple pizza"],
-        tim_discriminator: ["dance", "fresh-prince", "he-man", "mary-poppins"],
-    }
-    if (
-        message.channel.category.name == "FPT INTERNAL"
-        and message.channel.name == "general"
-    ):
-        if message.author.discriminator in user_discriminators.keys():
-            search_items = user_discriminators[message.author.discriminator]
-            search_idx = random.randint(0, len(search_items) - 1)
-            gif = find_gif(search_items[search_idx])
-            default_mssg = f"Ey {message.author.mention} bring me coffee."
-            await message.channel.send(default_mssg)
-            if gif:
-                await message.channel.send(gif)
+    mssg_chn = message.channel
+    if mssg_chn.category.name == "FPT INTERNAL" and mssg_chn.name == "general":
+        time_to_spam(message)
 
     messages_to_filter = [
         "hail hydra",
@@ -69,12 +106,10 @@ async def on_message(message):
     if any(mssg in mssg_contnt for mssg in messages_to_filter):
         gif = find_gif("hail-hydra")
         if gif:
-            await message.channel.send(gif)
+            await mssg_chn.send(gif)
         else:
-            await message.channel.send("Heil hydra (couldn't find gifs soz).")
+            await mssg_chn.send("Heil hydra (couldn't find gifs soz).")
 
 
-discord_key = os.getenv("DISCORD_TOKEN")
-gabri_discriminator = os.getenv("gabri_discriminator")
-tim_discriminator = os.getenv("tim_discriminator")
+keep_alive()
 client.run(discord_key)
